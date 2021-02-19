eba53c9c0efdcb3c88727f9c26404cf614034aa8
monkin77 <monkinsmurf@gmail.com>
2021-02-19 14:50:11 +0000

    Revert "Add condition of sum or mult"
    This reverts commit b2f97466dc7859be770ed69993517a8fc2b03c7b.
    
    Reverting commit

--- src/Application.java
+++ src/Application.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         String operation = scanner.nextLine();
         int a = scanner.nextInt();
         int b = scanner.nextInt();
